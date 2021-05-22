ceed86cae2226f3716e102404d0c5f01a4ffcf5f
CoolMineman <62723322+CoolMineman@users.noreply.github.com>
2021-05-22 13:48:24 -0500

    port to forge

---
+++ src/main/java/io/github/coolcrabs/augmentedautojump/ShutItForge.java
@@ -0,0 +1,10 @@
+package io.github.coolcrabs.augmentedautojump;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("augmentedautojump")
+public class ShutItForge {
+    public ShutItForge() {
+        //Why??????
+    }
+}
